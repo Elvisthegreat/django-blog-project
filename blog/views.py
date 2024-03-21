55d59ba661021a39dc4c58040823a6fb207a980f
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-21 04:36:19 +0000

    Adding css styling to our blog project

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # we can use .filter(author=1) or 2 or 3 to get a certain post and author
-    template_name = "post_list.html" # If we did want a different template name, then we can set it using template_name =  like we did in our example.
-
+    template_name = "blog/index.html"
+    paginate_by = 6
