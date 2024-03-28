bc3e1731f11085fb56879a677d27cdd2ff4f1275
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-28 19:16:48 +0000

    Working on open post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,27 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # we can use .filter(author=1) or 2 or 3 to get a certain post and author
     template_name = "blog/index.html"
     paginate_by = 6 # this tells to display 6 blog post at a time on the homepage, we change to any number
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
