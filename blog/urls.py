02e8f0082115fa072037da689577a868a8176509
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-29 18:26:26 +0000

    Comment

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.PostList.as_view(), name='home'), # as.view() for class-based view
+    path('<slug:slug>/', views.post_detail, name='post_detail'), # as for normal function no as.view() is required
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6 # this tells to display 6 blog post at a time on the homepage, we change to any number
 
-
+# function for our post_detail to dsiplay a single blog post
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
