fe106fca0be475f27ebf86facdfe9d4246b0951b
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-03-31 16:41:30 +0000

    Commented

--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,9 @@
 from . import views
 from django.urls import path
 
+"""check the name of your function then add for 
+about_me, use it for the views."""
+
 urlpatterns = [
     path('', views.about_me, name='about'),
 ]
