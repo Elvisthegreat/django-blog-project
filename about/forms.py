925729e1252b802ff34974eb621b8995e6b677e9
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-06 04:59:39 +0000

    Another way to display the fields

--- about/forms.py
+++ about/forms.py
@@ -6,3 +6,5 @@ class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
         fields = ('name', 'email', 'message')
+        # fields = '__all__'  # Include all fields from the model except the "read" field
+        # exclude = ['read']  # Exclude the "read" field from the form
