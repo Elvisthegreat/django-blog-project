551c5559c36827ccaa6c80711ae78582372544af
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-07 16:16:29 +0000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'), # as.view() for class-based view
-    path('<slug:slug>/', views.post_detail, name='post_detail'), # as for normal function no as.view() is required
+    path('<slug:slug>/', views.post_detail, name='post_detail'), 
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'), # urls for editing comment
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),# urls for deleting comment
 ]
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello, world! Am Elvis");
