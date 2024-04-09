13da9290a387d635bd33756dfba82688cc851fc2
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-09 22:04:51 +0000

    enable serving of image files

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -10,6 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts" 
     ) # Foreign Key. One user can write many posts, so this is a one-to-many or Foreign Key
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField() # text field
     created_on = models.DateTimeField(auto_now_add=True) # created time is the time of post entry.
     status = models.IntegerField(choices=STATUS, default=0) # The option choices use the STATUS constant to limit the integer choice to 0 or 1 and has mapped these two integers to "Draft" and "Published".
--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY") # my personal secret_key from env.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-elvisthegre-djangoblogp-xz16t2acql1.ws-eu110.gitpod.io','.herokuapp.com'] # Always add this '.herokuapp.com'
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]
