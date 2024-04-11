427ae2ec6a43679eea008b500cbc462a5d4f6a6c
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-11 16:50:00 +0000

    Testing with assertFalse

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -7,3 +7,7 @@ class TestCommentForm(TestCase):
     def test_form_is_valid(self): # defined a function
         comment_form = CommentForm({'body': 'This is a great post'}) # filled the form with string to make sure the comment form is valid
         self.assertTrue(comment_form.is_valid(), msg='Form is not valid') # Finally, using an assert to determine if the form is valid. Since the body field is required, and we have provided some content, the test should pass.
+
+    def test_form_is_invalid(self):
+        comment_form = CommentForm({'body': ''})
+        self.assertFalse(comment_form.is_valid(), msg='Form is valid')
