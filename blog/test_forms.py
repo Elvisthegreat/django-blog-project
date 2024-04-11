b3d3dfaca242230bfdad0eaac39cbfe09c21c496
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-11 13:24:19 +0000

    Comment

---
+++ blog/test_forms.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .forms import CommentForm
+
+
+class TestCommentForm(TestCase):
+
+    def test_form_is_valid(self): # defined a function
+        comment_form = CommentForm({'body': 'This is a great post'}) # filled the form with string to make sure the comment form is valid
+        self.assertTrue(comment_form.is_valid(), msg='Form is not valid') # Finally, using an assert to determine if the form is valid. Since the body field is required, and we have provided some content, the test should pass.
