a0a24e78747085231b6ef069f77d63c886e2b1f5
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-04-12 03:06:57 +0000

    Testing

--- about/test_forms.py
+++ about/test_forms.py
@@ -12,3 +12,36 @@ class TestCollaborateForm(TestCase):
             'message': 'Hello!'
         })
         self.assertTrue(form.is_valid(), msg="Form is not valid")
+
+    def test_form_is_valid_one(self):
+        """check that the form is not valid if we fail 
+        to populate one of the fields in the dictionary."""
+
+        form_one = CollaborateForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hey!'
+        })
+        self.assertFalse(form_one.is_valid(), msg="Form is not correctly populated")
+
+    def test_form_is_valid_two(self):
+        """check that the form is not valid if we fail 
+        to populate one of the fields in the dictionary."""
+
+        form_two = CollaborateForm({
+            'name': 'Blessing',
+            'email': 'test@amadin.com',
+            'message': ''
+        })
+        self.assertFalse(form_two.is_valid(), msg='Form is not correctly populated')
+
+    def test_form_is_valid_three(self):
+        """check that the form is not valid if we fail 
+        to populate one of the fields in the dictionary."""
+
+        form_three = CollaborateForm({
+            'name': 'Amadin',
+            'email': '',
+            'message': 'Ho!'
+        })
+        self.assertFalse(form_three.is_valid(), msg='Form is not correctly populated')
